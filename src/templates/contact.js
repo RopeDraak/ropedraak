90a4483a4b46d8542c31123336f51d81fdbc3284
Gulden Draak <guldendraakrope@gmail.com>
2019-11-10 13:58:24 -0500

    fixed contact, site title, and removed social bubbles

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -24,28 +24,6 @@ export default class Contact extends React.Component {
               }
               <div className="post-content">
                 {htmlToReact(_.get(this.props, 'pageContext.html'))}
-                <form name="contactForm" method="POST" netlifyHoneypot="bot-field" data-netlify="true" id="contact-form"
-                  className="contact-form">
-                  <p className="screen-reader-text">
-                    <label>Don't fill this out if you're human: <input name="bot-field" /></label>
-                  </p>
-                  <p className="form-row">
-                    <label className="form-label">Name *</label>
-                    <input type="text" name="name" placeholder="Your name..." className="form-input"/>
-                  </p>
-                  <p className="form-row">
-                    <label className="form-label">Email *</label>
-                    <input type="email" name="email" placeholder="Your email address..." className="form-input"/>
-                  </p>
-                  <p className="form-row">
-                    <label className="form-label">Message *</label>
-                    <textarea name="message" placeholder="Your message..." className="form-textarea" rows="7" />
-                  </p>
-                  <input type="hidden" name="form-name" value="contactForm" />
-                  <p className="form-row">
-                    <button type="submit" className="button">Send Message</button>
-                  </p>
-                </form>
               </div>
             </article>
             </Layout>
